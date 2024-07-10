c604d990bf7d51d16408159b72a1fd2898d036be
Douglas Silva <douglasitpro@gmail.com>
2024-07-10 09:40:06 -0300

    Criando choice para UF

--- gerenciamento/models.py
+++ gerenciamento/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from .choices import UF_CHOICES
 
-# Create your models here.
+
+class Fornecedor(models.Model):
+    nome = models.CharField(max_length=255)
+    cnpj_cpf = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=255)
+    bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+    uf = models.CharField(max_length=2, choices=UF_CHOICES, default='RJ')
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+    vendedor = models.CharField(max_length=255)
+    vendedor_telefone = models.CharField(max_length=15)
+
