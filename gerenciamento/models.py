aed5f12b73122bc9601d666996b278986e29269a
Douglas Silva <douglasitpro@gmail.com>
2024-07-10 12:41:08 -0300

    Crinado model Cliente

--- gerenciamento/models.py
+++ gerenciamento/models.py
@@ -14,3 +14,14 @@ class Fornecedor(models.Model):
     vendedor = models.CharField(max_length=255)
     vendedor_telefone = models.CharField(max_length=15)
 
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    cnpj_cpf = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=255)
+    bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+    uf = models.CharField(max_length=2, choices=UF_CHOICES, default='RJ')
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+    vendedor = models.CharField(max_length=255)
+
